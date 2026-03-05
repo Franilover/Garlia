dbd576e30679c99f034a1c50a829600f2293a74f
Franilover <franilover@proton.me>
2026-03-04 22:01:20 -0300

    asd

--- src/hooks/auth/useIsAdmin.ts
+++ src/hooks/auth/useIsAdmin.ts
@@ -5,8 +5,16 @@ export function useIsAdmin() {
   const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
-    supabase.auth.getSession().then(({ data }) => {
-      setIsAdmin(!!data.session);
+    supabase.auth.getSession().then(async ({ data }) => {
+      if (!data.session) return;
+
+      const { data: perfil } = await supabase
+        .from("perfiles")
+        .select("rol")
+        .eq("id", data.session.user.id)
+        .single();
+
+      setIsAdmin(perfil?.rol === "admin");
     });
   }, []);
 
