d3ad334ebe884a84aec72862e0d93f4ceafddf4c
Franilover <franilover@proton.me>
2026-03-21 15:13:56 -0300

    asd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <DataProvider>
             <ThemeProvider>
               <LightboxProvider>
-                <div className="flex-grow">
+                <div className="flex-grow md:pl-[68px]">
                   <AppLogic>
                     {children}
                   </AppLogic>
