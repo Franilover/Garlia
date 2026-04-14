ae4c526377c23d970d278328a350b93d4fe8de45
Franilover <franilover@proton.me>
2026-04-13 21:06:42 -0400

    ads

--- src/components/paginas/wiki/libros/biblioteca.tsx
+++ src/components/paginas/wiki/libros/biblioteca.tsx
@@ -28,13 +28,6 @@ const Biblioteca = () => {
 
   return (
     <div className="min-h-screen bg-bg-main pb-20">
-      <div className="max-w-6xl mx-auto pt-16 px-6">
-        <PageHeader
-          title="Biblioteca"
-          subtitle="Explora los relatos del mundo"
-          icon={<Book size={32} />}
-        />
-      </div>
 
       <div className="max-w-6xl mx-auto px-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
         {libros.map((libro, index) => (
