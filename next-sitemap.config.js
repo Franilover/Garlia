af3f0a13b8035d3faab22e88c3917cbac4d4acac
Franilover <franilover@proton.me>
2026-04-18 14:05:34 -0400

    ads

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,16 @@
 module.exports = {
   siteUrl: 'https://franilover.vercel.app',
   generateRobotsTxt: true,
-  stylesheet: '/sitemap.xsl', 
+  stylesheet: '/sitemap.xsl',
+  exclude: [], 
+  transform: async (config, path) => {
+    if (!path) return null;
+    
+    return {
+      loc: path,
+      changefreq: 'daily',
+      priority: 0.7,
+      lastmod: new Date().toISOString(),
+    };
+  },
 }
