a300386a87666e5b9b70f6d1da21dbae15b083ba
Franilover <franilover@proton.me>
2026-04-18 22:00:10 -0400

    asd

--- src/components/paginas/personal/sobre-mi.tsx
+++ src/components/paginas/personal/sobre-mi.tsx
@@ -126,7 +126,7 @@ export default function SobreMi() {
 
                       <div className="relative z-10 space-y-0.5 text-left">
                         <h4
-                          className="font-black text-m leading-snug"
+                          className="font-black text-l leading-snug"
                           style={{ color: "var(--primary)", letterSpacing: "-0.02em" }}
                         >{tool.title}</h4>
                       </div>
