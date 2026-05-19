0ed04645784ed79cec964f960169c20ae28f7c56
Franilover <franilover@proton.me>
2026-05-19 12:24:33 -0400

    ads

--- src/app/myself/jardin/page.tsx
+++ src/app/myself/jardin/page.tsx
@@ -1,4 +1,11 @@
-import DashboardPage from '@/components/paginas/myself/jardin/page';
-export default function Page() {
-  return <DashboardPage />;
+"use client";
+import { AdminOnly } from "@/components/forms/AdminOnly";
+import EditorEntidades from "@/components/paginas/myself/jardin/editorEntidades";
+
+export default function DashboardPage() {
+  return (
+    <AdminOnly>
+      <EditorEntidades />
+    </AdminOnly>
+  );
 }
