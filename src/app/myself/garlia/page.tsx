a2051d17fb1e615d1c4a9eb07591d5fb6a1fb844
Franilover <franilover@proton.me>
2026-05-19 13:36:39 -0400

    ads

--- src/app/myself/garlia/page.tsx
+++ src/app/myself/garlia/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { AdminOnly } from "@/components/forms/AdminOnly";
-import EditorEntidades from "@/components/paginas/myself/jardin/editorGarlia";
+import EditorEntidades from "@/components/paginas/myself/garlia/editorGarlia";
 
 export default function DashboardPage() {
   return (
