b6b1b47cfc56ae1fed5add2e41d74e70830da91f
Franilover <franilover@proton.me>
2026-05-21 20:18:35 -0400

    asd

--- src/components/forms/MarkdownEditor.tsx
+++ src/components/forms/MarkdownEditor.tsx
@@ -243,7 +243,7 @@ if (hMatch) {
 
       output.push(`
         <div class="callout link-list-block">
-          <div class="callout-title"><span class="callout-title-icon">🔗</span> ${customTitle}</div>
+          <div class="callout-title"><span class="callout-title-icon"></span> ${customTitle}</div>
           <div class="callout-body"><ul>${linksHtml}</ul></div>
         </div>`);
       continue;
