9452c49f34c75ee32eb429c84b82479868de885f
Franilover <franilover@proton.me>
2026-06-05 13:40:52 -0400

    sad

--- src/components/paginas/myself/garlia/EditorPersonaje.tsx
+++ src/components/paginas/myself/garlia/EditorPersonaje.tsx
@@ -835,6 +835,7 @@ export function FormularioPersonaje({
                       />
                     </div>
                   )}
+
                 </div>
               </div>
             </div>
